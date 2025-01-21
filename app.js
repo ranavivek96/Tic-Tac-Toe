577330be081745eb44544af56f7139200f5f758b
ranavivek96 <ranavivek9694@gmail.com>
2025-01-21 15:58:49 +0530

    Add Winner

--- app.js
+++ app.js
@@ -1,5 +1,9 @@
 let boxes = document.querySelectorAll(".box");
 let resetBtn = document.querySelector("#reset-btn");
+let newGameBtn = document.querySelector("new-btn");
+let msgContainer = document.querySelector(".msg-container")
+let msg = document.querySelector("#msg");
+
 
 let turnO = true; //playerO, playerX
 
@@ -31,3 +35,26 @@ boxes.forEach((box) => {
         checkWinner();
     })
 })
+
+// For check Winner
+
+const showWinner = (winner) => {
+    msg.innerText=`Congratulations, Winner is ${winner}`;
+    msgContainer.classList.remove("hide");
+}
+
+const checkWinner = () => {
+    for(let pattern of winPatterns) {
+        // console.log(pattern[0], pattern[1], pattern[2]);
+        let pos1val = boxes[pattern[0]].innerText;
+        let pos2val = boxes[pattern[1]].innerText;
+        let pos3val = boxes[pattern[2]].innerText;
+
+        if(pos1val != "" && pos2val != "" && pos3val != "") {
+            if(pos1val === pos2val && pos2val === pos3val) {
+                console.log("winner", pos1val);
+                showWinner();
+            }
+        }
+    }
+}
